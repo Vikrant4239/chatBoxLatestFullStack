89908722710c1251fb56ae507eea03dda4ce2257
Vikrant4239 <bhardwazvikrant83@gmail.com>
2023-04-05 19:03:18 +0530

    ChatBox Backend Added

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const sequelize = require('./util/database');
+const cors = require('cors');
+
+const User = require('./models/user');
+const Chat = require('./models/chat');
+
+require('dotenv').config();
+
+const app = express();
+app.use(cors())
+app.use(bodyParser.json({ extended: false }));
+const userRoute = require('./routers/user');
+const chatRoute = require('./routers/chat');
+
+app.use('/user',userRoute);
+app.use('/users', chatRoute);
+
+User.hasMany(Chat);
+Chat.belongsTo(User);
+
+sequelize
+.sync()
+
+.then(result=>{
+   app.listen(3000);
+})
+.catch(err=>{
+    console.log(err);
+}); 
---
+++ frontend/chat/chat.js
@@ -0,0 +1,42 @@
+const chatForm = document.getElementById('chat-form');
+const chatMessageInput = document.getElementById('chat-message');
+const userList = document.getElementById('user-list');
+const chatMessages = document.getElementById('chat-messages');
+
+chatForm.addEventListener('submit',async (event) => {
+    event.preventDefault();
+    const token = localStorage.getItem('token');
+    let message = {text:chatMessageInput.value};
+    const response = await axios.post("http://localhost:3000/users/chat",message,{headers: {'Authentication' :token}});
+    console.log(response);
+    chatMessageInput.value = '';
+  });
+
+window.addEventListener('load', ()=>{
+  getusers();
+  getmessages();
+})
+
+async function getusers(){
+    const response = await axios.get("http://localhost:3000/user/signup");
+    console.log(response.data.users);
+    const userlist = response.data.users;
+    userlist.forEach((user) => {
+      const userElement = document.createElement('div');
+      userElement.textContent = user.name+" joined";
+      userList.appendChild(userElement);
+    });
+}
+
+async function getmessages(){
+ const response = await axios.get("http://localhost:3000/users/chat");
+ console.log('response',response);
+ console.log(response.data.message);
+ chatHistory = response.data.message;
+  chatHistory.forEach((chat) => {
+    const chatMessageElement = document.createElement('div');
+    chatMessageElement.textContent = `${chat.userName}: ${chat.message}`;
+    chatMessages.appendChild(chatMessageElement);
+  });
+}
+
